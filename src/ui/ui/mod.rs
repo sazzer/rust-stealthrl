0c01b09306e75fc331bc0f4e71ccb3d83c52484d
Graham Cox <graham@grahamcox.co.uk>
2014-10-15 11:55:21 +0000

    Ability to get the dimensions of the UI

--- src/ui/ui/mod.rs
+++ src/ui/ui/mod.rs
@@ -1,3 +1,5 @@
+extern crate ncurses;
+
 #[doc = "The actual User Interface"]
 pub struct UI;
 
@@ -7,10 +9,34 @@ impl Drop for UI {
     Tidy up everything about the UI when it closes
     "]
     fn drop(&mut self) {
+        ncurses::getch();
         debug!("Destroying UI");
+        ncurses::endwin();
     }
 }
 
+impl UI {
+    #[experimental]
+    #[doc = "
+    Get the width of the user interface in cells
+    
+    # Returns
+    The number of cells wide
+    "]
+    pub fn width(&self) -> uint {
+        ncurses::COLS as uint
+    }
+    #[experimental]
+    #[doc = "
+    Get the heigth of the user interface in cells
+    
+    # Returns
+    The number of cells tall
+    "]
+    pub fn height(&self) -> uint {
+        ncurses::LINES as uint
+    }
+}
 #[experimental]
 #[doc = "
 Actually create the User Interface
@@ -20,5 +46,6 @@ The object representing the user interface
 "]
 pub fn create_ui() -> UI {
     debug!("Creating UI");
+    ncurses::initscr();
     UI
 }
